4334220808bc80d42f8e1a75e8fd06ee0b5d3d11
Sumeet jha <sumitjha365@gmail.com>
2025-08-27 10:31:42 +0530

    Update environment configuration for database connection and Google API key. Modify .env.development.local to include MongoDB connection details and enhance dbConnection function in index.ts for potential SRV connection support.

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,6 +4,7 @@ import { NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 export const dbConnection = async () => {
   const dbConfig = {
     url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+    // url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
   };
 
   if (NODE_ENV !== 'production') {
